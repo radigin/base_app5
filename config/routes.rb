da561504b89846fe652a0d2dba5db80cb55d3b2e
radigin <radigin@192.168.1.3>
2016-09-18 21:51:22 +0300

    Первый коммит

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users do
+    member do
+      get :activate
+    end
+  end
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
